be0d258654fc02a88bc06d6effd0ca633707a489
gearsgrinding <hualex@seas.upenn.edu>
2017-04-05 18:46:37 -0400

    hotfix

--- public/graphs.js
+++ public/graphs.js
@@ -1,20 +1,13 @@
 google.charts.load('current', {'packages':['line']});
-var xChoice = document.getElementById("stringX");
-var yChoice = document.getElementById("stringY");
-var timeLength = document.getElementById("timeSpan")
-document.getElementById('render_chart').addEventListener("click", function(){
-  var strX = xChoice.options[xChoice.selectedIndex].text;
-  var strY = yChoice.options[yChoice.selectedIndex].text;
-  var endTime = (new Date()).getTime();
-  var timeOffSet = findTimeOffset(timeLength);
-  var beginTime = endTime- timeOffset;
-  drawChart(strX, strY, beginTime, endTime);
-});
-google.charts.setOnLoadCallback(drawChart);
 
-function drawChart(x, y, beginTime, endTime) {
- var xCol = 0;
- var yCol = 0;
+document.getElementById('render_chart').addEventListener("click",buttonClickDraw);
+google.charts.setOnLoadCallback(buttonClickDraw);
+
+function buttonClickDraw(strX,strY,formTime) {
+   drawChart(strX, strY, formTime);
+}
+
+function drawChart(x, y, formTime) {
  var data = new google.visualization.DataTable();
  switch (x) {
   case "Time":
@@ -22,15 +15,12 @@ function drawChart(x, y, beginTime, endTime) {
   break;
   case "Temperature":
   data.addColumn('number', 'Temperature');
-  xCol = 1;
   break;
   case "Light":
   data.addColumn('number', 'Light');
-  xCol = 2;
   break;
   case "pH":
   data.addColumn('number', 'pH');
-  xCol = 3;
   break;
 }
 switch (y) {
@@ -39,19 +29,16 @@ switch (y) {
   break;
   case "Temperature":
   data.addColumn('number', 'Temperature');
-  yCol = 1;
   break;
   case "Light":
   data.addColumn('number', 'Light');
-  yCol = 2;
   break;
   case "pH":
   data.addColumn('number', 'pH');
-  yCol = 3;
   break;
 }
 
-data.addRows(isolateCols(xCol, yCol, data(beginTime, endTime)));
+data.addRows(dataTest(x, y, formTime));
 
 var options = {
  chart: {
@@ -78,41 +65,50 @@ function createArray(length) {
   return arr;
 }
 
-function isolateCols(xCol, yCol, dataArr) {
-  var dataArray = createArray(dataArr.length, 2)
+function filterData(dataArr, endTime, startTime, xCol, yCol) {
+  var count = 0;
+  for(i = 0; i < dataArr.length; i++){
+    if(dataArr[i].date >= startTime && dataArr[i].date <= endTime) {
+      count++;
+    }
+  }
+  var dataArray = createArray(count, 2);
+  var index = 0;
   for (i = 0; i < dataArr.length; i++) {
-    dataArray[i][0] = dataArr[i][xCol];
-    dataArray[i][1] = dataArr[i][yCol];
+    if(dataArr[i].date >= startTime && dataArr[i].date <= endTime) {
+      switch(xCol) {
+        case "Time":
+        dataArray[index][0] = dataArr[i].date;
+        break;
+        case "Temperature":
+        dataArray[index][0] = dataArr[i].temp;
+        break;
+        case "Light":
+        dataArray[index][0] = dataArr[i].light;
+        break;
+        case "pH":
+        dataArray[index][0] = dataArr[i].ph;
+        break;
+      }
+      switch(yCol) {
+        case "Time":
+        dataArray[index][1] = dataArr[i].date;
+        break;
+        case "Temperature":
+        dataArray[index][1] = dataArr[i].temp;
+        break;
+        case "Light":
+        dataArray[index][1] = dataArr[i].light;
+        break;
+        case "pH":
+        dataArray[index][1] = dataArr[i].ph;
+        break;
+      }
+      index++;
+    }
   }
   return dataArray;
 }
-
-function findTimeOffset(timeLengthValue) {
-  var offset = 0;
-  var hourOffset = 3600000;
-  switch(timeLengthValue) {
-    case 1:
-    offset = hourOffset * 6;
-    break;
-    case 2:
-    offset = hourOffset * 24;
-    break;
-    case 3:
-    offset = hourOffset * 24*3;
-    break;
-    case 4:
-    offset = hourOffset * 24*7;
-    break;
-    case 5:
-    offset = hourOffset * 24*14;
-    break;
-    case 6:
-    offset = hourOffset * 24*30;
-    break;
-    case 7:
-    offset = hourOffset * 24*60;
-    break;
-  }
 
   function drawData() {
     console.log("test");
@@ -122,5 +118,48 @@ function findTimeOffset(timeLengthValue) {
     var response = JSON.parse(xhttp.responseText);
   }
 
- 
-}
+  function dataTest(xCol, yCol, formTime){
+
+    var durationInMinutes = 0;
+    switch(formTime) {
+      case "1":
+      durationInMinutes = 6*60;
+      break;
+      case "2":
+      durationInMinutes = 24*60;
+      break;
+      case "3":
+      durationInMinutes = 24*3*60;
+      break;
+      case "4":
+      durationInMinutes = 24*7*60;
+      break;
+      case "5":
+      durationInMinutes = 24*7*2*60;
+      break;
+      case "6":
+      durationInMinutes = 24*30*60;
+      break;
+      case "7":
+      durationInMinutes = 24*30*2*60;
+      break;
+    }
+    var MS_PER_MINUTE = 60000;
+    var date = new Date();
+    var myStartDate = (date - durationInMinutes * MS_PER_MINUTE);
+    var url = "http://localhost:8080/data";
+    var dataTry = httpGet(url);
+    console.log(JSON.parse(dataTry));
+    var dataArr = filterData(dataTry, date, myStartDate, xCol, yCol);
+    return dataArr;
+  };
+
+
+  function httpGet(theUrl)
+  {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+  }
+
